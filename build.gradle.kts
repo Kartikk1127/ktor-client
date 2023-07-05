baed03c0c695c298e3932acd24beca7e35b3dd4e
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-07-05 15:56:31 +0530

    ktor client made

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.8.22"
+    id("io.ktor.plugin") version "2.3.2"
+    kotlin("plugin.serialization") version "1.5.30"
+}
+
+group = "com.example"
+version = "0.0.1"
+application {
+    mainClass.set("com.example.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation ("ch.qos.logback:logback-classic:1.2.6")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ src/main/kotlin/com/example/plugins/Main.kt
@@ -0,0 +1,67 @@
+package com.example.plugins
+
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.http.*
+import io.ktor.client.HttpClient
+import io.ktor.client.call.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.util.*
+//import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+@Serializable
+data class Matrix(val rows: Int, val columns: Int, val values: List<List<Int>>)
+
+@Serializable
+data class MatrixMultiplicationRequest(
+    val matrix1: Matrix,
+    val matrix2: Matrix
+)
+
+@Serializable
+data class MatrixMultiplicationResponse(
+    val result: Matrix
+)
+
+
+
+
+
+
+suspend fun postMatrixMultiplicationRequest(matrix1: Matrix, matrix2: Matrix): HttpResponse{
+    val client = HttpClient(CIO){
+        install(ContentNegotiation){
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+            })
+        }
+    } //a client is represented by the http client class
+    val request = MatrixMultiplicationRequest(matrix1, matrix2)
+//    val requestBody = Matrix(matrix1.rows, matrix2.columns, matrix1.values)
+    val response: HttpResponse = client.post("http://localhost:8080/api/matrix-multiply"){
+        contentType(ContentType.Application.Json)
+        setBody(request)
+    }
+    println(response.status)
+    client.close()
+
+    return response
+}
+
+@OptIn(InternalAPI::class)
+suspend fun main(){
+    val matrix1 = Matrix(2, 2, listOf(listOf(1, 2), listOf(3, 4)))
+    val matrix2 = Matrix(2, 2, listOf(listOf(5, 6), listOf(7, 8)))
+    val response = postMatrixMultiplicationRequest(matrix1, matrix2)
+//    val json = response.
+    val finalResponse: Matrix = response.body()
+
+    println(finalResponse)
+}
